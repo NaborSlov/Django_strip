2983c6cda6768e4218109b10ae1c949594500d31
Никита <nik.ysmanof@mail.ru>
2023-02-13 18:36:31 +0500

    fix url

--- strip/urls.py
+++ strip/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from strip.views import BuyView, GetItemView
 
 urlpatterns = [
-    path('buy/<int:id>', BuyView.as_view()),
-    path('order/<int:id>', GetItemView.as_view()),
+    path('buy/<int:id>', BuyView.as_view(), name="buy"),
+    path('order/<int:id>', GetItemView.as_view(), name="order"),
 ]
