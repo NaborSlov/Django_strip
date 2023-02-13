1b75e42e2ba06a86ba61e0d60a1ec86055602a7d
Никита <nik.ysmanof@mail.ru>
2023-02-13 18:41:55 +0500

    .env

--- django_strip/settings.py
+++ django_strip/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ru=y)55o)5bn!_6rxmkh@s7(rbw)(eogq-5yqe0qd6)(++q(ss'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
